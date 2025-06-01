6f659fe3fc26db4cdf24b48f272c2522f34f6538
Nikita Bogoslovskiy <nikita-beeline@yandex.ru>
2025-06-02 01:27:45 +0300

    add di

--- internal/di/di.go
+++ internal/di/di.go
@@ -1,1 +1,15 @@
 package di
+
+import (
+	"github.com/NikitaBogoslovskiy/quotes/internal/handlers"
+	"github.com/NikitaBogoslovskiy/quotes/internal/services"
+	"github.com/NikitaBogoslovskiy/quotes/internal/stores"
+)
+
+func InitializeQuotesHandler() handlers.QuotesHandler {
+	quotesStore := stores.NewQuotesStore()
+	quotesService := services.NewQuotesService(quotesStore)
+	quotesHandler := handlers.NewQuotesHandler(quotesService)
+
+	return quotesHandler
+}
