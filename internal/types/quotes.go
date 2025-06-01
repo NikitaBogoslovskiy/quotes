e83a5007a98f3a642ba044a2b6c4a4dd55f50355
Nikita Bogoslovskiy <nikita-beeline@yandex.ru>
2025-06-01 22:30:07 +0300

    implement types and their validation

--- internal/types/quotes.go
+++ internal/types/quotes.go
@@ -1,1 +1,87 @@
 package types
+
+import "fmt"
+
+type Id int64
+
+func (id Id) Validate() error {
+	if id <= 0 {
+		return fmt.Errorf("Id cannot be non-positive")
+	}
+
+	return nil
+}
+
+type Author string
+
+func (a Author) Validate() error {
+	if len(a) == 0 {
+		return fmt.Errorf("Author field cannot be empty")
+	}
+
+	return nil
+}
+
+type Quote string
+
+func (q Quote) Validate() error {
+	if len(q) == 0 {
+		return fmt.Errorf("Quote field cannot be empty")
+	}
+
+	return nil
+}
+
+type QuoteData struct {
+	Id     Id     `json:"id,omitempty"`
+	Author Author `json:"author,omitempty"`
+	Quote  Quote  `json:"quote,omitempty"`
+}
+
+type CreateQuoteRequest struct {
+	Author Author `json:"author"`
+	Quote  Quote  `json:"quote"`
+}
+
+func (cqr CreateQuoteRequest) Validate() error {
+	err := cqr.Author.Validate()
+	if err != nil {
+		return err
+	}
+
+	err = cqr.Quote.Validate()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type CreateQuoteResponse struct {
+	Ok      bool   `json:"ok"`
+	Message string `json:"message,omitempty"`
+	Id      Id     `json:"id,omitempty"`
+}
+
+type GetAllQuotesResponse struct {
+	Ok      bool        `json:"ok"`
+	Message string      `json:"message,omitempty"`
+	Quotes  []QuoteData `json:"quotes,omitempty"`
+}
+
+type GetRandomQuoteResponse struct {
+	Ok      bool      `json:"ok"`
+	Message string    `json:"message,omitempty"`
+	Quote   QuoteData `json:"quote,omitempty"`
+}
+
+type GetQuoteByAuthorResponse struct {
+	Ok      bool      `json:"ok"`
+	Message string    `json:"message,omitempty"`
+	Quote   QuoteData `json:"quote,omitempty"`
+}
+
+type DeleteQuoteByIdResponse struct {
+	Ok      bool   `json:"ok"`
+	Message string `json:"message,omitempty"`
+}
