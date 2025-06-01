a8f685708f45f818e2ba1643da398bb9cbe4bcb9
Nikita Bogoslovskiy <nikita-beeline@yandex.ru>
2025-06-02 01:27:59 +0300

    configure http and routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/NikitaBogoslovskiy/quotes/cmd/routes"
+	"github.com/NikitaBogoslovskiy/quotes/internal/di"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	service := routes.NewService(routes.Service{
+		QuotesHandler: di.InitializeQuotesHandler(),
+	})
+	service.LoadRoutes(router)
+
+	http.Handle("/", router)
+	http.ListenAndServe(":8080", nil)
 }
--- cmd/routes/routes.go
+++ cmd/routes/routes.go
@@ -1,1 +1,22 @@
 package routes
+
+import (
+	"github.com/NikitaBogoslovskiy/quotes/internal/handlers"
+	"github.com/gorilla/mux"
+)
+
+type Service struct {
+	QuotesHandler handlers.QuotesHandler
+}
+
+func NewService(service Service) *Service {
+	return &service
+}
+
+func (s *Service) LoadRoutes(router *mux.Router) {
+	quotes := router.PathPrefix("/quotes").Subrouter()
+	quotes.HandleFunc("", s.QuotesHandler.Create).Methods("POST")
+	quotes.HandleFunc("", s.QuotesHandler.Get).Methods("GET")
+	quotes.HandleFunc("/random", s.QuotesHandler.GetRandom).Methods("GET")
+	quotes.HandleFunc("/{id}", s.QuotesHandler.Delete).Methods("DELETE")
+}
